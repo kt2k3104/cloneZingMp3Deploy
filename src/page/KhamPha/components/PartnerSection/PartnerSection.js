4edb76c1ba4f75d4104da3b2208e02c378704954
kt2k3104 <127589801+kt2k3104@users.noreply.github.com>
2023-10-01 02:47:43 +0700

    fix no^'t img khampha

--- src/page/KhamPha/components/PartnerSection/PartnerSection.js
+++ src/page/KhamPha/components/PartnerSection/PartnerSection.js
@@ -8,22 +8,54 @@ function PartnerSection() {
     <div className={cx('wrapper')}>
       <h3 className={cx('title')}>ĐỐI TÁC ÂM NHẠC</h3>
       <div className={cx('content')}>
-        <img src="/assets/img/khampha/doitacamnhac/1.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/2.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/3.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/4.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/5.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/6.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/7.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/8.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/9.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/10.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/11.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/12.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/13.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/14.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/15.png" alt="img" />
-        <img src="/assets/img/khampha/doitacamnhac/16.png" alt="img" />
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/sony.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/genie.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/FUGA.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/believe.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/yg.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/beggers.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/taihe.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/orcahrd.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/universal-1.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/ingrooves.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/route-note.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/monstercat.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/stone-music.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/hikoon.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/danal.png" alt="img" />
+        </div>
+        <div>
+          <img src="https://static-zmp3.zmdcdn.me/skins/zmp3-v6.1/images/partner_logo/empire.png" alt="img" />
+        </div>
       </div>
     </div>
   );
