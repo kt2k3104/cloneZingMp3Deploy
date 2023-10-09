bbf1207d5aa2aae0a882d63da8e1fab6bce484f1
khai <kt2k3104@gmail.com>
2023-10-09 17:13:06 +0700

    /

--- src/const.js
+++ src/const.js
@@ -1,3 +1,3 @@
 // export const REACT_APP_API_URL = 'http://localhost:9000/';
-// export const REACT_APP_API_URL = 'https://music-app-server-w5pu.onrender.com/';
-export const REACT_APP_API_URL = 'http://146.190.80.230:9000/';
+export const REACT_APP_API_URL = 'https://music-app-server-w5pu.onrender.com/';
+// export const REACT_APP_API_URL = 'http://146.190.80.230:9000/';
--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = 'http://localhost:9000';
-// const URL = 'https://music-app-server-w5pu.onrender.com';
-const URL = 'http://146.190.80.230:9000/';
+const URL = 'https://music-app-server-w5pu.onrender.com';
+// const URL = 'http://146.190.80.230:9000/';
 
 const token = localStorage.getItem('access_token');
 
